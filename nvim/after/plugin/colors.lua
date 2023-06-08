ed2a5e78c926848d5386833018505083de477213
Ryan Sadowski <ryan.sadowski@knighttrans.com>
2023-06-07 20:13:27 -0700

    .

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -19,14 +19,14 @@ vim.g.nord_bold = false
 require('nord').set()
 
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
     background = {
         -- :h background
         light = "latte",
-        dark = "mocha",
+        dark = "macchiato",
     },
     transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+    show_end_of_buffer = true,      -- shows the '~' characters after the end of buffers
     term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
         enabled = false,            -- dims the background color of inactive window
@@ -55,11 +55,15 @@ require("catppuccin").setup({
     custom_highlights = {},
     integrations = {
         cmp = true,
-        gitsigns = true,
-        nvimtree = true,
         telescope = true,
-        notify = false,
-        mini = false,
+        treesitter = true,
+        --        lualine = true,
+        harpoon = true,
+        mason = true,
+        --dap = {
+        --  enabled = true,
+        --enable_ui = true
+        -- }
         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
     },
 })
